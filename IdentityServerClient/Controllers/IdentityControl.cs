93cb7c6d30a9cc2ad4c3a87dfdd1c70311032162
Rstar <yanhongxing@bestwise.cc>
2018-03-19 18:52:10 +0800

    Init

---
+++ IdentityServerClient/Controllers/IdentityControl.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerClient.Controllers
+{
+    public class IdentityControl: ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
